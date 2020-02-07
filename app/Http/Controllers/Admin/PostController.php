88cb60bf430308848d7717f6bf05245f58c5fde9
mdshajibazher <mdshajibazher@gmail.com>
2020-02-07 08:54:01 +0600

    Post Create Functionality Added

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Tag;
 use App\Post;
+use App\Category;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Brian2694\Toastr\Facades\Toastr;
+use Illuminate\Support\Facades\Auth;
+use Intervention\Image\Facades\Image;
+use App\Http\Requests\Admin\PostValidationRequest;
 
 class PostController extends Controller
 {
@@ -26,7 +34,9 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.post.create', compact('categories','tags'));
     }
 
     /**
@@ -35,9 +45,43 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostValidationRequest $request)
     {
-        //
+
+        if($request->hasFile('image')){
+            //get form image
+           $image = $request->file('image');
+           $slug = Str::slug($request->title);
+           $current_date = Carbon::now()->toDateString();
+           //get unique name for image
+           $image_name = $slug."-".$current_date.".".$image->getClientOriginalExtension();
+           //location for new image 
+           $image_location = public_path('image/post/'.$image_name);
+           //resize image for category and upload temp 
+           $post_image = Image::make($image)->resize(1600,1066)->save($image_location);
+
+       }
+        $post = new Post;
+        $post->user_id = Auth::id();
+        $post->title = $request->title;
+        $post->slug = Str::slug($request->post);
+        $post->image = $image_name;
+        $post->body = $request->body;
+        if(isset($request->status)){
+            $post->status = $request->status;
+        }else{
+            $post->status = 0;
+        }
+        $post->is_approved = 1;
+        $post->save();
+
+        $post->categories()->attach($request->categories);
+        $post->tags()->attach($request->tags);
+
+        Toastr::success('Post Inserted To Databse successful!', 'success');
+        return redirect(route('admin.post.index'));
+
+
     }
 
     /**
--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -1,17 +1,22 @@
 @extends('layouts.backend.app')
-@section('title', 'Create Category')
+@section('title', 'Create Post')
 @push('css')
-<link rel="stylesheet" href="{{asset('public/assets/backend/plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css')}}">
+<!-- Bootstrap Select Css -->
+<link href="{{asset('public/assets/backend/plugins/bootstrap-select/css/bootstrap-select.css')}}" rel="stylesheet" />
 @endpush
 
 @section('content')
+
+<form action="{{route('admin.post.store')}}" method="POST" enctype="multipart/form-data">
+    @csrf
+
 <div class="row clearfix">
-    <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
+    <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
         <div class="card">
             <div class="header">
                 <h2>
-                    ADD NEW Category
-                    <small>With floating label</small>
+                    ADD NEW POST
+                    
                 </h2>
 
             </div>
@@ -25,42 +30,116 @@
                     </ul>
                 </div>
             @endif
-            <form action="{{route('admin.category.store')}}" method="POST" enctype="multipart/form-data">
-                @csrf
+            
                     <div class="form-group form-float">
                         <div class="form-line">
-                            <input type="text" id="name" class="form-control" name="name">
-                            <label class="form-label">Category Name</label>
+                            <input type="text" id="title" class="form-control" name="title">
+                            <label class="form-label">Post Title</label>
                         </div>
                     </div>
                     <div class="form-group form-float">
                         <div class="form-line">
+                            <label for="image">Image</label>
                             <input type="file" id="image" class="form-control" name="image">
-                            {{-- <label class="form-label">Category Image</label> --}}
+                            
                         </div>
+                    </div>
+                    <div class="form-group">
+                            
+                            <input type="checkbox" id="publish" class="filled-in" name="status" value="1">
+                            <label for="publish">publish</label>
+                        
                     </div>
                     
+                    
+
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    CATEGORIES AND TAGS
+                    
+                </h2>
+
+            </div>
+            <div class="body">
+        
+            
+                    <div class="form-group form-float">
+                        <div class="form-line {{$errors->has('categories') ? 'focused error' : ''}}">
+                            <label for="categories">Select Category</label>
+                            <select name="categories[]" class="form-control show-tick" id="categories" data-live-search="true" multiple>
+                                @foreach ($categories as $item)
+                            <option value="{{$item->id}}">{{$item->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group form-float">
+                        <div class="form-line {{$errors->has('tags') ? 'focused error' : ''}}">
+                            <label for="tags">Select Tags</label>
+                            <select name="tags[]" class="form-control show-tick" id="tags" data-live-search="true" multiple>
+                                @foreach ($tags as $item)
+                            <option value="{{$item->id}}">{{$item->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    
                     <button type="submit" class="btn btn-primary m-t-15 waves-effect">submit</button>
-                    <a class="btn btn-danger m-t-15 waves-effect" href="{{route('admin.category.index')}}">back</a>
-                </form>
+                  
+
             </div>
         </div>
     </div>
 </div>
 
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <h2>
+                    Body
+                </h2>
+
+            </div>
+            <div class="body">
+
+                <textarea name="body" id="tinymce" cols="30" rows="10"></textarea>
+                
+            </div>
+        </div>
+    </div>
+</div>
+</form>
 
 @endsection
 @push('js')
-<script src="{{asset('public/assets/backend/plugins/jquery-datatable/jquery.dataTables.js')}}"></script>
-<script src="{{asset('public/assets/backend/plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js')}}"></script>
-<script src="{{asset('public/assets/backend/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js')}}"></script>
-<script src="{{asset('public/assets/backend/plugins/jquery-datatable/extensions/export/buttons.flash.min.js')}}"></script>
-<script src="{{asset('public/assets/backend/plugins/jquery-datatable/extensions/export/jszip.min.js')}}"></script>
-<script src="{{asset('public/assets/backend/plugins/jquery-datatable/extensions/export/pdfmake.min.js')}}"></script>
-<script src="{{asset('public/assets/backend/plugins/jquery-datatable/extensions/export/vfs_fonts.js')}}"></script>
-<script src="{{asset('public/assets/backend/plugins/jquery-datatable/extensions/export/buttons.html5.min.js')}}"></script>
-<script src="{{asset('public/assets/backend/plugins/jquery-datatable/extensions/export/buttons.print.min.js')}}"></script>
-<script src="{{asset('public/assets/backend/js/pages/tables/jquery-datatable.js')}}"></script>
+<!-- TinyMCE -->
+<script src="{{asset('public/assets/backend/plugins/tinymce/tinymce.js')}}"></script>
+<script>
+    //TinyMCE
+    tinymce.init({
+        selector: "textarea#tinymce",
+        theme: "modern",
+        height: 300,
+        plugins: [
+            'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'emoticons template paste textcolor colorpicker textpattern imagetools'
+        ],
+        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        toolbar2: 'print preview media | forecolor backcolor emoticons',
+        image_advtab: true
+    });
+    tinymce.suffix = ".min";
+    tinyMCE.baseURL = '{{asset('public/assets/backend/plugins/tinymce')}}';
+</script>
 @endpush
 
 
